d0abbce30f2bfc4d8124346f161715708153c275
Petros Kouzoupis <peterkuzu@gmail.com>
2025-02-02 17:25:07 +0200

    Iteration 2

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
-export const menu = [
+export const menuData = [
     {
         name: "Pizza",
         ingredients: "pepperoni, mushromm, mozarella",
-        price: 14
+        price: 14,
+        image: "./images/pizza.png"
     },
     {
         name: "Hamburger",
         ingredients: "beef, cheese, lettuce",
-        price: 12
+        price: 12,
+        image: "./images/burger.png"
     },
     {
         name: "Beer",
         ingredients: "grain, hops, yeast, water",
-        price: 12
+        price: 12,
+        image: "./images/beer.png"
     }
 ]
--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-import { menu } from "./data";
+import { menuData } from "./data.js";
 
-const menuItems = document.getElementById("menu-items")
-
-function renderMenu() {
-    const menuString = ``
-    for(let menuItm of menu) {
-        menuString += `
-        <img src="" alt="" />
-          <div class="menu-item-desc">
-            <h2>${menuItm.name}</h2>
-            <p>${menuItm.ingredients}</p>
-            <p>$${menuItm.price}</p>
-          </div>
-          <button aria-label="Add item to order">+</button>
-        `
-    }
-    console.log(menuString)
-    menuItems.innerHTML = menuString
+function getMenu() {
+  let menuHTML = ``
+  menuData.forEach(menu => {
+    menuHTML += `
+        <div class="menu-item">
+            <img src=${menu.image} alt="" />
+            <div class="menu-item-desc">
+             <h2>${menu.name}</h2>
+             <p>${menu.ingredients}</p>
+             <p>$${menu.price}</p>
+           </div>
+           <button aria-label="Add item to order">+</button>
+        </div>
+    `
+  })
+  return menuHTML
 }
-renderMenu()
+
+function render() {
+  document.getElementById("menu-items").innerHTML = getMenu()
+}
+
+render()

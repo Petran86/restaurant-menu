ad0b879c5c10471a3d1bd65af16dd9f93d2282c6
Petros Kouzoupis <peterkuzu@gmail.com>
2025-02-15 14:58:40 +0200

    Added the order section
    Next tasks: add the remove functionality, add the modal form, completion of the order

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
 export const menuData = [
     {
+        id: "1",
         name: "Pizza",
         ingredients: "pepperoni, mushromm, mozarella",
         price: 14,
         image: "./images/pizza.png"
     },
     {
+        id: "2",
         name: "Hamburger",
         ingredients: "beef, cheese, lettuce",
         price: 12,
         image: "./images/burger.png"
     },
     {
+        id: "3",
         name: "Beer",
         ingredients: "grain, hops, yeast, water",
         price: 12,
--- index.js
+++ index.js
@@ -1,5 +1,51 @@
 import { menuData } from "./data.js";
 
+let totalPrice = 0
+
+document.addEventListener("click", e => {
+  if(e.target.dataset.pizza) {
+    handlePizzaClick(e.target.dataset.pizza)
+  }
+  else if(e.target.dataset.hamburger) {
+    handleBurgerClick(e.target.dataset.hamburger)
+  }
+  else if(e.target.dataset.beer) {
+    handleBeerClick(e.target.dataset.beer)
+  }
+})
+
+function handlePizzaClick(menuId) {
+  const targetMenuObj = menuData.filter(menu => {
+    return menu.id === menuId
+  })[0]
+  renderOrder(targetMenuObj)
+}
+
+function handleBurgerClick(menuId) {
+  const targetMenuObj = menuData.filter(menu => {
+    return menu.id === menuId
+  })[0]
+  renderOrder(targetMenuObj)
+}
+
+function handleBeerClick(menuId) {
+  const targetMenuObj = menuData.filter(menu => {
+    return menu.id === menuId
+  })[0]
+  renderOrder(targetMenuObj)
+}
+
+function renderOrder(orderObj) {
+    document.getElementById("order-items").innerHTML += `
+      <ul>
+        <li>${orderObj.name} <span class="price">$${orderObj.price}</span></li>
+      </ul>
+    `
+  totalPrice += orderObj.price
+  document.getElementById("total-price").textContent = `$${totalPrice}`
+  document.getElementById("order").classList.remove("hidden")
+}
+
 function getMenu() {
   let menuHTML = ``
   menuData.forEach(menu => {
@@ -13,15 +59,15 @@ function getMenu() {
              <p class="menu-item-price">$${menu.price}</p>
             </div>
            </div>
-           <button class="menu-add-btn" aria-label="Add ${menu.name} to order">+</button>
+           <button class="menu-add-btn" aria-label="Add ${menu.name} to order" data-${menu.name}="${menu.id}">+</button>
         </div>
     `
   })
   return menuHTML
 }
 
-function render() {
+function renderMenu() {
   document.getElementById("menu-items").innerHTML = getMenu()
 }
 
-render()
+renderMenu()
